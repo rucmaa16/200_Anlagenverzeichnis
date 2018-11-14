3ab011ca02e259ba92ead92d867e8c64c31c069d
Fabian Fritz <frifaa16@htl-kaindorf.ac.at>
2018-11-14 10:27:06 +0100

    GUI,BL,CellRenderer

--- Anlagenverzeichnis/src/model/AvCellRenderer.java
+++ Anlagenverzeichnis/src/model/AvCellRenderer.java
@@ -5,10 +5,51 @@
  */
 package model;
 
-/**
- *
- * @author Rucki
- */
-public class AvCellRenderer {
-    
+import data.AvBL;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+
+public class AvCellRenderer  implements TableCellRenderer{
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel label = new JLabel();
+        label.setOpaque(true);
+        AvModel model = (AvModel) table.getModel();
+        AvBL a = model.getRow(row);
+        AvEnum enumIndex = AvEnum.values()[column];
+       
+        switch (enumIndex) {
+            case BEZEICHNUNG:
+                label.setText(a.getBezeichnung());
+                break;
+            case AK:
+                label.setText(Double.parseDouble(a.getAK()));
+                break;
+            case ND:
+                label.setText(Double.parseDouble(a.getND()));
+                break;
+            case BISHND:
+                label.setText(Double.parseDouble(a.getBish_ND()));
+                break;
+            case AFABISHER:
+                label.setText(Double.parseDouble(a.getAfa_bisher()));
+                break;
+            case ND:
+                label.setText(Double.parseDouble(a.getND()));
+                break;
+             case ND:
+                label.setText(Double.parseDouble(a.getND()));
+                break;
+           
+        }
+
+        return label;
+
+    }
 }
+
--- Anlagenverzeichnis/src/model/AvModel.java
+++ Anlagenverzeichnis/src/model/AvModel.java
@@ -5,10 +5,32 @@
  */
 package model;
 
+import data.AvBL;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Rucki
  */
-public class AvModel {
+public class AvModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    AvBL getRow(int row) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
