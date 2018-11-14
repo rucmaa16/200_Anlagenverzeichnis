2cc625c8a2002b495b87f9fdf9b04814092a73df
Marc Ruckenstuhl <rucmaa16@htl-kaindorf.ac.at>
2018-11-14 10:15:37 +0100

    Enum und Model(ohne Save)

--- Anlagenverzeichnis/src/model/AvEnum.java
+++ Anlagenverzeichnis/src/model/AvEnum.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author Rucki
- */
+
 public enum AvEnum {
+    BEZEICHNUNG("Bezeichnung"),
+    AK("AK"),
+    INBETRIEBNAHME("Inbetriebnahme"),
+    ND("ND"),
+    BISHND("bish. ND"),
+    AFABISHER("AfA bisher"),
+    WERTVORAFA("Wert vor AfA"),
+    AFADJ("AfA d. J."),
+    BW("Bw 31.12");
+    
+    private String name;
+
+    private AvEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
     
 }
--- Anlagenverzeichnis/src/model/AvModel.java
+++ Anlagenverzeichnis/src/model/AvModel.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author Rucki
- */
-public class AvModel {
-    
+import data.AvBL;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class AvModel extends AbstractTableModel {
+
+    private List<AvBL> liste;
+
+    public AvModel() {
+        this.liste = new LinkedList<>();
+    }
+
+    @Override
+    public int getRowCount() {
+        return this.liste.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return AvEnum.values().length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        AvBL a = this.liste.get(rowIndex);
+
+        AvEnum enumIndex = AvEnum.values()[columnIndex];
+        switch (enumIndex) {
+            case BEZEICHNUNG:
+                return a.getBezeichnung();
+            case AK:
+                return a.getAK();
+            case INBETRIEBNAHME:
+                return a.getInbetriebnahme();
+            case ND:
+                return a.getND();
+//            case BISHND:
+//                return a.getBish_ND();
+//            case AFABISHER:
+//                return a.getAfa_bisher();
+//            case WERTVORAFA:
+//                return a.getWert_vor_Afa();
+//            case AFADJ:
+//                return a.getAfA_d_J();
+//            case BW:
+//                return a.getBW_31_12();
+            default:
+                return "?";
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return AvEnum.values()[column].getName();
+    }
+
+    public AvBL getRow(int index) {
+        return this.liste.get(index);
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    public void load(File f) throws IOException {
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(fr = new FileReader(f));
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                AvBL a = new AvBL(line);
+                liste.add(a);
+            }
+        } catch (FileNotFoundException e) {
+
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+        }
+    }
 }
